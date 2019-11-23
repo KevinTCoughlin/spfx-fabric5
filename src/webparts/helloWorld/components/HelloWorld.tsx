5e14917f8d4fd134a12d40d347ada277a4d1ae4c
Kevin Coughlin <KevinTCoughlin@users.noreply.github.com>
2019-11-22 21:18:08 -0800

    Use createTheme

--- src/webparts/helloWorld/components/HelloWorld.tsx
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -1,17 +1,51 @@
 import * as React from 'react';
 import { IHelloWorldProps } from './IHelloWorldProps';
-import { Button } from 'office-ui-fabric-react';
-import { loadTheme } from 'office-ui-fabric-react';
+import { Button, Customizer, ICustomizerProps, ITheme, createTheme } from 'office-ui-fabric-react';
+
+export const theme: ITheme = createTheme({
+  palette: {
+    themePrimary: "#6264a7",
+    themeLighterAlt: "#f7f7fb",
+    themeLighter: "#e1e1f1",
+    themeLight: "#c8c9e4",
+    themeTertiary: "#989ac9",
+    themeSecondary: "#7173b0",
+    themeDarkAlt: "#585a95",
+    themeDark: "#4a4c7e",
+    themeDarker: "#37385d",
+    neutralLighterAlt: "#f8f8f8",
+    neutralLighter: "#f4f4f4",
+    neutralLight: "#eaeaea",
+    neutralQuaternaryAlt: "#dadada",
+    neutralQuaternary: "#d0d0d0",
+    neutralTertiaryAlt: "#c8c8c8",
+    neutralTertiary: "#c2c2c2",
+    neutralSecondary: "#858585",
+    neutralPrimaryAlt: "#4b4b4b",
+    neutralPrimary: "#333",
+    neutralDark: "#272727",
+    black: "#1d1d1d",
+    white: "#fff"
+  },
+  semanticColors: {
+    bodyBackground: "#fff",
+    bodyText: "#333"
+  }
+});
 
 export default class HelloWorld extends React.Component<IHelloWorldProps, {}> {
   public render(): React.ReactElement<IHelloWorldProps> {
-
-    loadTheme({ palette: { themePrimary: '#ee1100', themeLighterAlt: '#ff1a08', themeLighter: '#ff3222', themeLight: '#ff1a08', themeTertiary: '#71afe5', themeSecondary: '#2b88d8', themeDarkAlt: '#d50f00', themeDark: '#d50f00', themeDarker: '#bb0d00', neutralLighterAlt: '#f8f8f8', neutralLighter: '#f4f4f4', neutralLight: '#eaeaea', neutralQuaternaryAlt: '#dadada', neutralQuaternary: '#d0d0d0', neutralTertiaryAlt: '#c8c8c8', neutralTertiary: '#c2c2c2', neutralSecondary: '#858585', neutralPrimaryAlt: '#4b4b4b', neutralPrimary: '#333333', neutralDark: '#272727', black: '#1d1d1d', white: '#ffffff', } });
-
+    const themeCustomizations: ICustomizerProps = {
+      settings: {
+        theme
+      }
+    };
+    
     return (
-      <div>
+      <Customizer {...themeCustomizations}>
+        <h1>Hello world</h1>
         <Button text="Test" />
-      </div>
+      </Customizer>
     );
   }
 }
